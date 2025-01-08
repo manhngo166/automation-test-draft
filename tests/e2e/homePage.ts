b36d3233f1d7f7c9ac61250885fff8818130c461
Manh Ngo <manhngo166@gmail.com>
2025-01-08 17:23:51 +0700

    test: update auto test for home page

---
+++ tests/e2e/homePage.ts
@@ -0,0 +1,43 @@
+import { type Page, type Locator } from "@playwright/test";
+
+class HomePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  getHeaderElement(): Locator {
+    return this.page.locator("header");
+  }
+
+  getLogoHeaderLink(): Locator {
+    return this.getHeaderElement().locator("a");
+  }
+
+  getToggleDarkThemeButton(): Locator {
+    return this.page.locator('button[aria-label="Toggle dark mode"]');
+  }
+
+  getFooterElement(): Locator {
+    return this.page.locator(".w-full.container.max-w-6xl.px-5.self-center");
+  }
+
+  getLogoFooterLink(): Locator {
+    return this.getFooterElement().locator("a");
+  }
+
+  async getPageTitle(): Promise<string> {
+    return await this.page.title();
+  }
+
+  async getCurrentTheme(): Promise<string> {
+    return (await this.page.locator("html").getAttribute("class")) || "";
+  }
+
+  getLoadMoreButton(): Locator {
+    return this.page.locator("button", { hasText: "Load more" });
+  }
+}
+
+export default HomePage;
